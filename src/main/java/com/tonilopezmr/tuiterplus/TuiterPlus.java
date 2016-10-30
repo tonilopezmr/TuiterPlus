3a6c71e6c7066e3a8881144ca390103b9e8725a5
Antonio López Marín <tonilopezmr@gmail.com>
2016-10-30 10:00:01 +0100

    Created empty project

---
+++ src/main/java/com/tonilopezmr/tuiterplus/TuiterPlus.java
@@ -0,0 +1,9 @@
+package com.tonilopezmr.tuiterplus;
+
+public class TuiterPlus {
+
+  public static void main(String... args) {
+    System.out.print("Hello Codurance!");
+  }
+
+}
---
+++ src/test/java/com/tonilopezmr/tuiterplus/TuiterPlusShould.java
@@ -0,0 +1,25 @@
+package com.tonilopezmr.tuiterplus;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TuiterPlusShould {
+
+  @Test
+  public void
+  show_greeting_example(){
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(output));
+
+    TuiterPlus.main();
+
+    assertThat(output.toString(), is("Hello Codurance!"));
+  }
+
+
+}
